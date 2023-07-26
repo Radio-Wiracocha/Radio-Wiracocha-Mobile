48d7d229214833b2b3e27d40e2492c6a67e0481c
AdriCBueno <adrianacc.bueno@gmail.com>
2023-07-26 02:15:08 -0400

    Radio updated, rows added

--- src/screens/LibraryScreen.tsx
+++ src/screens/LibraryScreen.tsx
@@ -1,25 +1,57 @@
-import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+import { StyleSheet, Text, TouchableOpacity, View, Image, FlatList } from 'react-native'
 import React from 'react'
 import { Ionicons } from '@expo/vector-icons';
+
+// Datos ficticios para ilustrar el ejemplo
+const DATA = [
+  { id: '1', src: 'https://f.rpp-noticias.io/2019/02/15/753296descarga-7jpg.jpg' },
+  { id: '2', src: 'https://f.rpp-noticias.io/2019/02/15/753296descarga-7jpg.jpg' },
+  { id: '3', src: 'https://f.rpp-noticias.io/2019/02/15/753296descarga-7jpg.jpg' },
+  { id: '4', src: 'https://f.rpp-noticias.io/2019/02/15/753296descarga-7jpg.jpg' },
+  { id: '5', src: 'https://f.rpp-noticias.io/2019/02/15/753296descarga-7jpg.jpg' },
+];
 
 const LibraryScreen = () => {
     return (
         <View style={styles.container}>
-            <TouchableOpacity style={styles.row}>
-                <Ionicons name="headset" color='#f48225' size={25} />
-                <Text style={styles.item}>Playlist</Text>
-            </TouchableOpacity>
-            <TouchableOpacity style={styles.row}>
-                <Ionicons name="ios-pencil" color='#f48225' size={25} />
-                <Text style={styles.item}>Artist</Text>
-            </TouchableOpacity>
-            <TouchableOpacity style={styles.row}>
-                <Ionicons name="disc" color='#f48225' size={25} />
-                <Text style={styles.item}>Albums</Text>
-            </TouchableOpacity>
-            <TouchableOpacity style={styles.row}>
-                <Ionicons name="folder" color='#f48225' size={25} />
-                <Text style={styles.item}>Folders</Text>
+            <Text style={styles.title}>Locutores</Text>
+            <FlatList
+                data={DATA}
+                horizontal
+                renderItem={({ item }) => (
+                    <TouchableOpacity onPress={() => {/* Navegar a detalles del locutor */}}>
+                        <Image style={styles.image} source={{ uri: item.src }} />
+                    </TouchableOpacity>
+                )}
+                keyExtractor={item => item.id}
+            />
+            
+            <Text style={styles.title}>Emisoras</Text>
+            <FlatList
+                data={DATA}
+                horizontal
+                renderItem={({ item }) => (
+                    <TouchableOpacity onPress={() => {/* Navegar a detalles de la emisora */}}>
+                        <Image style={styles.image} source={{ uri: item.src }} />
+                    </TouchableOpacity>
+                )}
+                keyExtractor={item => item.id}
+            />
+
+            <Text style={styles.title}>Podcasts</Text>
+            <FlatList
+                data={DATA}
+                horizontal
+                renderItem={({ item }) => (
+                    <TouchableOpacity onPress={() => {/* Navegar a detalles del podcast */}}>
+                        <Image style={styles.image} source={{ uri: item.src }} />
+                    </TouchableOpacity>
+                )}
+                keyExtractor={item => item.id}
+            />
+
+            <TouchableOpacity style={styles.button}>
+                <Text style={styles.buttonText}>Reproducir emisora actual</Text>
             </TouchableOpacity>
         </View>
     )
@@ -33,16 +65,25 @@ const styles = StyleSheet.create({
         marginTop: 20,
         padding: 5
     },
-    item: {
-        fontSize: 20,
-        paddingStart: 10,
-        color: '#f48225'
+    title: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        marginVertical: 10,
     },
-    row: {
-        flexDirection: 'row',
-        margin: 5
+    image: {
+        width: 100,
+        height: 100,
+        marginRight: 10,
     },
-    icon: {
-
-    }
+    button: {
+        marginTop: 20,
+        backgroundColor: '#f48225',
+        padding: 10,
+        alignItems: 'center',
+        borderRadius: 5,
+    },
+    buttonText: {
+        color: 'white',
+        fontWeight: 'bold',
+    },
 })
