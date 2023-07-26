9d33f7c2fb33649c772083ccfda4ca583187a65b
AdriCBueno <adrianacc.bueno@gmail.com>
2023-07-26 02:05:48 -0400

    Messages improved

--- src/screens/MessageScreen.tsx
+++ src/screens/MessageScreen.tsx
@@ -1,12 +1,36 @@
-import React, { useState } from 'react'
-import { View, StyleSheet, ToastAndroid } from 'react-native'
-import { Input, Button } from 'react-native-elements'
-import { getFirestore, collection, addDoc, serverTimestamp } from '@firebase/firestore';
+import React, { useState, useEffect } from 'react'
+import { View, StyleSheet, ToastAndroid, FlatList, Text } from 'react-native'
+import { Input, Button, ListItem } from 'react-native-elements'
+import { getFirestore, collection, addDoc, serverTimestamp, query, orderBy, limit, onSnapshot } from '@firebase/firestore';
 import firebaseConfig from '../constants/Firebase';
+import { Timestamp } from '@firebase/firestore';
+
+type Message = {
+    id: string;
+    text: string;
+    timestamp: Timestamp;
+    user: string;
+}
 
 const MessageScreen = () => {
     const [message, setMessage] = useState('');
+    const [messages, setMessages] = useState<Message[]>([]);
     const db = getFirestore(firebaseConfig.app);
+
+    useEffect(() => {
+        const messagesRef = collection(db, 'messages');
+        const q = query(messagesRef, orderBy('timestamp', 'desc'), limit(5));
+        
+        const unsubscribe = onSnapshot(q, (snapshot) => {
+            const newMessages: Message[] = snapshot.docs.map((doc) => ({
+                id: doc.id,
+                ...doc.data(),
+            }) as Message);
+            setMessages(newMessages);
+        });
+        
+        return () => unsubscribe();  // Limpiamos el listener al desmontar el componente
+    }, [db]);
 
     const sendMessage = async () => {
         if(message.trim() === '') return;
@@ -31,12 +55,30 @@ const MessageScreen = () => {
 
     return (
         <View style={styles.container}>
+            <Text style={styles.item}>¡Envía un mensaje al locutor!</Text>            
             <Input 
                 placeholder="Escribe tu mensaje..." 
                 value={message} 
                 onChangeText={setMessage}
             />
             <Button title="Enviar mensaje" onPress={sendMessage} />
+
+            {messages.length === 0 ? (
+                <Text style={styles.noMessageText}>Ningún mensaje enviado aún</Text>
+            ) : (
+                <FlatList
+                    data={messages}
+                    renderItem={({ item }) => (
+                        <ListItem bottomDivider>
+                            <ListItem.Content>
+                                <ListItem.Title>{item.text}</ListItem.Title>
+                                <ListItem.Subtitle>{item.user}</ListItem.Subtitle>
+                            </ListItem.Content>
+                        </ListItem>
+                    )}
+                    keyExtractor={item => item.id}
+                />
+            )}
         </View>
     )
 }
@@ -48,5 +90,15 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'center',
         padding: 20,
+    },
+    noMessageText: {
+        textAlign: 'center',
+        marginTop: 20,
+    },
+    item: {
+        fontSize: 20,
+        paddingBottom: 20,
+        color: '#f48225',
+        textAlign: 'center'
     },
 })
